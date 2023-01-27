15c4cc28042f6f6169e95fe89105d4d4cdab9f4f
lhogan3 <hoganliam3@gmail.com>
2023-01-27 15:14:50 +0000

    learning baout http get

---
+++ httpgetchallenge01.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+        "fmt"
+        "io"
+        "log"
+        "net/http"
+)
+
+func main() {
+        // create a GET request to the page
+    resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/pikachu")
+
+    // check for errors
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // the client must close the response body when finished
+    defer resp.Body.Close()
+
+    // read the content of the body with "ReadAll()"
+    body, err := io.ReadAll(resp.Body)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // print received data to the console
+    fmt.Println(string(body))
+}
